dc101e862686f8f376757cfd6b30e24b369063b4
Andrew Pochapsky <andrew.pochapsky@gmail.com>
2023-08-27 00:28:23 +0000

    Add circom highlighting and diagnostic windows (#6)

--- .config/nvim/lua/andrew/lsp.lua
+++ .config/nvim/lua/andrew/lsp.lua
@@ -71,3 +71,6 @@ require("lspconfig").sumneko_lua.setup({
         },
     },
 })
+
+vim.o.updatetime = 250
+vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
